97efa70b393ab0c3f000cad21af6d3492fef4331
DaEpicBacon <127553457+DaEpicBacon@users.noreply.github.com>
2023-06-27 12:00:56 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,64 @@
-// Initialize Firebase
-const firebaseConfig = {
-  // Your Firebase configuration
-};
-firebase.initializeApp(firebaseConfig);
+// Import the required MongoDB driver
+const MongoClient = require('mongodb').MongoClient;
 
-// Get a reference to the storage service
-const storage = firebase.storage();
+// Connection URL and database name
+const url = 'mongodb+srv://fwsj4y:uo-hyffq3@cluster0.8i9vcnx.mongodb.net/?retryWrites=true&w=majority'; // Replace with your MongoDB connection URL
+const dbName = 'mydatabase'; // Replace with your database name
 
-// Handle file upload
-const fileInput = document.getElementById('file-input');
-fileInput.addEventListener('change', (event) => {
-  const file = event.target.files[0];
-  const storageRef = storage.ref();
-  const fileRef = storageRef.child(file.name);
-  fileRef.put(file)
-    .then((snapshot) => {
-      console.log('File uploaded successfully!');
-      // Get the download URL of the uploaded file
-      fileRef.getDownloadURL()
-        .then((url) => {
-          console.log('Download URL:', url);
-          // Display the uploaded image
-          const imgElement = document.createElement('img');
-          imgElement.src = url;
-          document.body.appendChild(imgElement);
-        })
-        .catch((error) => {
-          console.error('Error getting download URL:', error);
-        });
-    })
-    .catch((error) => {
-      console.error('Error uploading file:', error);
-    });
+// Connect to the MongoDB server
+MongoClient.connect(url, function(err, client) {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+    return;
+  }
+
+  console.log('Connected to MongoDB successfully');
+
+  const db = client.db(dbName);
+
+  // Perform database operations
+  const collection = db.collection('your_collection_name'); // Replace with your collection name
+
+  // Insert a document
+  const document = { name: 'John Doe', age: 30 };
+  collection.insertOne(document, function(err, result) {
+    if (err) {
+      console.error('Error inserting document:', err);
+    } else {
+      console.log('Document inserted successfully:', result.insertedId);
+    }
+  });
+
+  // Find documents
+  collection.find({}).toArray(function(err, documents) {
+    if (err) {
+      console.error('Error finding documents:', err);
+    } else {
+      console.log('Found documents:', documents);
+    }
+  });
+
+  // Update a document
+  const filter = { name: 'John Doe' };
+  const update = { $set: { age: 35 } };
+  collection.updateOne(filter, update, function(err, result) {
+    if (err) {
+      console.error('Error updating document:', err);
+    } else {
+      console.log('Document updated successfully');
+    }
+  });
+
+  // Delete a document
+  const deleteFilter = { name: 'John Doe' };
+  collection.deleteOne(deleteFilter, function(err, result) {
+    if (err) {
+      console.error('Error deleting document:', err);
+    } else {
+      console.log('Document deleted successfully');
+    }
+  });
+
+  // Close the MongoDB connection
+  client.close();
 });
